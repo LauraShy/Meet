408d39cf688c05af55818129a0d8727ac4d5c9fa
LauraShy <78038876+LauraShy@users.noreply.github.com>
2021-11-10 13:00:11 -0500

    4.5 acceptance testing scenario 1 & 2

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,8 +1,10 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import App from '../App';
 import { mockData } from '../mock-data';
+import CitySearch from '../CitySearch';
+import { extractLocations } from '../api';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
@@ -21,26 +23,28 @@ defineFeature(feature, test => {
     then('the user should see the list of upcoming events from all locations', () => {
       AppWrapper.update();
 
-      expect(AppWrapper.find('.event-card')).toHaveLength(mockData.length);
+      expect(AppWrapper.find('.event-card').hostNodes()).toHaveLength(mockData.length);
     });
   });
 
   test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
+    
+    let CitySearchWrapper;
     given('the main page is open', () => {
-
+      CitySearchWrapper = shallow(<CitySearch updateEvents={() => {}} locations={locations} />);
     });
 
     when('the user starts typing in the city textbox', () => {
-
+      CitySearchWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } });
     });
 
     then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
-
+      expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(2);
     });
   });
 
   test('User can select a city from the suggested list', ({ given, and, when, then }) => {
-    given('user was typing “Richmond” in the city textbox', () => {
+    given('user was typing “Berlin” in the city textbox', () => {
 
     });
 
@@ -48,11 +52,11 @@ defineFeature(feature, test => {
 
     });
 
-    when('the user selects a city (e.g., “Richmond, Virginia”) from the list', () => {
+    when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
 
     });
 
-    then('their city should be changed to that city (i.e., “Richmond, Virginia”)', () => {
+    then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
 
     });
 
