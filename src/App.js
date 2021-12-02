bb8c7ba0ebfcf6288827ccc1c81f26587f830ce8
LauraShy <78038876+LauraShy@users.noreply.github.com>
2021-12-02 14:57:44 -0500

    Add scatterchart

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { ErrorAlert } from './Alert';
 import { WarningAlert } from './Alert';
 import WelcomeScreen from './WelcomeScreen';
 import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 import { mockData } from './mock-data';
 
 class App extends Component {
@@ -21,6 +22,16 @@ class App extends Component {
     currentLocation: "all",
     showWelcomeScreen: undefined,
   }
+
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
 
   updateEvents = (location, eventCount) => {
     getEvents().then((events) => {
@@ -100,6 +111,22 @@ class App extends Component {
           />
           <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         </div>
+        <h4>Events in each city</h4>
+
+         <ScatterChart
+          width={400}
+          height={400}
+          margin={{
+            top: 20, right: 20, bottom: 20, left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis type="category" dataKey="city" name="city" />
+          <YAxis type="number" dataKey="number" name="number of events" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
+
         <EventList events={this.state.events} />
         <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
       </div>
